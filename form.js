20fed836735b9fb37b3edf55ee9b78ee007bdda7
jorellana <j_orellana28@berkeley.edu>
2018-01-15 11:46:35 -0800

    Changed contact form input to variables in form.js

--- form.js
+++ form.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
         alert("Text!");
     });
     $("#submit").click(function(){
-    	alert("Name: " + $("#name").val() + "\n" +
-    		  "Phone: " + $("#phone").val() + "\n" +
-    		  "Email: " + $("#email").val());
+    	var name = $("#name").val();
+    	var phone = $("#phone").val();
+    	var email = $("#email").val();
+    	var comments = $("#comments").val();
+    	alert("Name: " + name + "\n" +
+    		  "Phone: " + phone + "\n" +
+    		  "Email: " + email) + "\n" +
+    		  "Comments: " + comments);
     });
 });
