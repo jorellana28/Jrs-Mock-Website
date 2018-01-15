884842d9dcbdab2dcd210b961d4daa850f9346b3
jorellana <j_orellana28@berkeley.edu>
2018-01-14 19:44:34 -0800

    Contact form creates alert (test)

--- form.js
+++ form.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $("#btn1").click(function(){
         alert("Text!");
     });
+    $("#submit").click(function(){
+    	alert("Name: " + $("#name").val() + "\n"
+    		  "Phone: " + $("#phone").val() + "\n"
+    		  "Email: " + $("#email").val())
+    });
 });
